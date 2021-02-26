891da41da8638b5c80fbd35f1efe2b5de91dcd4a
ahmetyasinu <ahmetyasinuzun@hotmail.com>
2021-02-26 16:03:33 +0300

    mailsender

---
+++ src/main/java/com/mail/sender/Feedback.java
@@ -0,0 +1,40 @@
+package com.mail.sender;
+
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
+
+public class Feedback {
+
+    @NotNull
+    private String name;
+    @Email
+    private String email;
+    @Min(10)
+    private String feedback;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getFeedback() {
+        return feedback;
+    }
+
+    public void setFeedback(String feedback) {
+        this.feedback = feedback;
+    }
+}
---
+++ src/main/java/com/mail/sender/FeedbackController.java
@@ -0,0 +1,45 @@
+package com.mail.sender;
+
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.ValidationException;
+
+@RestController
+@RequestMapping("/feedback")
+public class FeedbackController {
+
+    private final EmailConfig emailConfig;
+
+    public FeedbackController(EmailConfig emailConfig) {
+        this.emailConfig = emailConfig;
+    }
+
+    @PostMapping
+    public void sendFeedback(@RequestBody Feedback feedback, BindingResult bindingResult){
+        if(bindingResult.hasErrors()){
+            throw new ValidationException("feedback is not valid");
+        }
+
+        JavaMailSenderImpl mailSender =new JavaMailSenderImpl();
+        mailSender.setHost(this.emailConfig.getHost());
+        mailSender.setPort(this.emailConfig.getPort());
+        mailSender.setUsername(this.emailConfig.getUsername());
+        mailSender.setPassword(this.emailConfig.getPassword());
+
+        SimpleMailMessage mailMessage =new SimpleMailMessage();
+        mailMessage.setFrom(feedback.getEmail());
+        mailMessage.setTo("Ahmetyasinuzun12@gmail.com");
+        mailMessage.setSubject("New FeedBack From"+feedback.getName());
+        mailMessage.setText(feedback.getFeedback());
+        mailSender.send(mailMessage);
+
+
+
+    }
+}
